645f33a45fffa5b2561f10179cd498ada3e0d102
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-02-06 12:42:52 -0800

    Added theme colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,22 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  mode: "jit",
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        black: "#1E1E1E",
+        white: "#FFFFFF",
+        blue: "#5FB2F7",
+      },
+      fontFamily: ["Poppins", sans-serif],
+    },
   },
   plugins: [],
 }
