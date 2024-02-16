0c20089459000673ce417116413605833392d1f3
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-02-16 13:58:47 -0800

    Technologies section asset import

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,17 +1,23 @@
 import brandLogo from "./brandLogo.svg"
-import reactLogo from "./react.svg"
 import menu from "./menu.svg"
 import close from "./close.png"
 import growth from "./growthSymbol.svg"
 import globe from "./worldhands.svg"
 import testImage from "./test.png"
+import htmlLogo from "./html.svg"
+import cssLogo from "./css.svg"
+import jsLogo from "./javascript.svg"
+import reactLogo from "./react.svg"
 
 export {
     brandLogo,
-    reactLogo,
     menu,
     close,
     growth,
     globe,
-    testImage
+    testImage,
+    htmlLogo,
+    cssLogo,
+    jsLogo,
+    reactLogo
 };
