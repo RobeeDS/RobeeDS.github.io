2bfef59ff388a658fb45ef9e62d1146f5f8d454a
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-02-23 13:50:57 -0800

    Imported additional images

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,12 +2,17 @@ import brandLogo from "./brandLogo.svg"
 import menu from "./menu.svg"
 import close from "./close.png"
 import growth from "./growthSymbol.svg"
-import globe from "./worldhands.svg"
+import globe from "./handportrait.png"
 import testImage from "./test.png"
 import htmlLogo from "./html.svg"
 import cssLogo from "./css.svg"
 import jsLogo from "./javascript.svg"
 import reactLogo from "./react.svg"
+import linkedinLogo from "./linkedin.svg"
+import githubLogo from "./github.png"
+import justplayit from "./justplayit.png"
+import hoobank from "./hoobank.png"
+import firstPortfolio from "./firstPortfolio.png"
 
 export {
     brandLogo,
@@ -19,5 +24,10 @@ export {
     htmlLogo,
     cssLogo,
     jsLogo,
-    reactLogo
+    reactLogo,
+    linkedinLogo,
+    githubLogo,
+    justplayit,
+    hoobank,
+    firstPortfolio
 };
