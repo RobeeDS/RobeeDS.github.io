1948f23dd6e23dd3b978108797a4d4b3c00ed365
robeeds <71895118+RobeeDS@users.noreply.github.com>
2024-02-23 13:52:18 -0800

    Added contact form actions, changed css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,21 +4,23 @@ const Contact = () => {
   return (
     <div id='contact' className='font-poppins font-normal flex flex-1 justify-center contact-card rounded-[50px]'>
       <container className={`m-6`}>
-        <form action='https://send.pageclip.co/ZHvMZSYcBnzvTOkNnWViIJjQRMKksyTc' method='post' className=''>
+        <form action='https://send.pageclip.co/ZHvMZSYcBnzvTOkNnWViIJjQRMKksyTc/contact-form' method='post' className=''>
           <div className='flex flex-row xxs:flex-wrap md:flex-nowrap'>
             {/* Left */}
-            <div className='text-white flex flex-col p-6 md:min-w-[50%]'>
+            <div className='text-white flex flex-1 flex-col p-6 md:min-w-[50%]'>
               <p className='xxs:text-[30px] lg:text-[54px] font-semibold'>Let's Talk</p>
-              <p className='xxs:text-[16px] md:text-[24px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum facilisis leo vel fringilla est ullamcorper eget.</p>
+              <p className='xxs:text-[16px] md:text-[24px]'>Are you ready to bring your website vision to life? Fill out the form below to get in touch with me, and let's discuss how we can elevate your online presence together.</p>
             </div>
 
             {/* Right */}
             <div className='text-white xxs:border-t-2 xxs:w-full md:border-t-0 md:border-l-2 flex flex-col p-6 md:min-w-[50%]'>
-              <p className='text-white '>Name</p>
-              <input className='text-black rounded-md' />
-              <p className='text-white  pt-2'>Email</p>
-              <input className='text-black rounded-md' />
-              <button className='bg-blue text-black px-4 py-2.5 mt-2 rounded-full max-w-[90px]'>Submit</button>
+              <p className='text-white text-[18px] pt-2.5 pb-1'>Name</p>
+              <input type='name' name='name' className='rounded-md p-1.5 bg-transparent border-[1px]' required />
+              <p className='text-white text-[18px] pt-2.5 pb-1'>Email</p>
+              <input type='email' name='email' className='rounded-md p-1.5 bg-transparent border-[1px]' required />
+              <p className='text-white text-[18px] pt-2.5 pb-1'>Message</p>
+              <input type='text' name='message' className='rounded-md p-1.5 pb-1 bg-transparent border-[1px]' required />
+              <button className='bg-blue text-black px-4 py-2.5 mt-4 rounded-full max-w-[90px]'>Send</button>
             </div>
           </div>
         </form>
